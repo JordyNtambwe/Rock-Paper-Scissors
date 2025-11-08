4b5d594c0c8a22fb0c217ff37f1611ed8c69774c
JordyNtambwe <jordyntambwe@gmail.com>
2025-11-08 14:38:05 -0700

    using the Math.random() method, I created a function 'getComputerChoice()' to return either 'rock, paper', or 'scissors'

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const choice = Math.random();
+  if (choice <= 1/3) {
+    return 'rock';
+  } else if (choice > 1/3 && choice <= 2/3) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+console.log(getComputerChoice());
